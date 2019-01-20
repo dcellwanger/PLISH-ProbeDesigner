4cf99fed8661b244c421948c812eb0023db998c8
dcellwanger <dc.ellwanger.dev@gmail.com>
2019-01-20 01:54:47 -0800

    updated readme

--- probeDesigner.py
+++ probeDesigner.py
@@ -15,7 +15,7 @@ from plishMain import main
 
 # Run tool in debug mode? For devel use only.
 _debug = False
-_version = '0.3.2 (Aug 2018)'
+_version = '0.4.0 (Jan 2019)'
 
 ###############################################################################
 # Set environment vars
